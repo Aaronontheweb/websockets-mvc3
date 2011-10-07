758180a7ad70f25ad7efd3dfc5d0b0cb1c29e78d
Aaron Stannard <aaron@aaronstannard.com>
2011-10-06 17:22:40 -0700

    Adding a new class to make it easier to work with the WebSocket server at a modular level

---
+++ WebSocketServer/SocketServerManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using SuperWebSocket;
+
+namespace TerminalSocketServer
+{
+    public class SocketServerManager
+    {
+        private List<WebSocketSession> m_Sessions = new List<WebSocketSession>();
+        private List<WebSocketSession> m_SecureSessions = new List<WebSocketSession>();
+        private object m_SessionSyncRoot = new object();
+        private object m_SecureSessionSyncRoot = new object();
+        private Timer m_SecureSocketPushTimer;
+    }
+}
